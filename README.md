2f08468723d09882d57d06c266b9e0e5fd928b67
Toru Ito <ir52578@gmail.com>
2016-11-05 18:25:32 +0900

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # libigl-Demo-Mac
+It is a simple demo to display the mesh using the libigl(https://github.com/libigl/libigl) in macOS.
+
+### Installation of the library
+- sudo port install glfw eigen3
+
+### Screen Shot
+![ScreenShot](ScreenShot.jpg)
---
+++ libigl-Demo-Mac/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <igl/readOFF.h>
+#include <igl/viewer/Viewer.h>
+
+Eigen::MatrixXd V;
+Eigen::MatrixXi F;
+
+int main(int argc, char *argv[])
+{
+    // Load a mesh in OFF format
+    igl::readOFF("/Users/user1/Documents/libigl/tutorial/shared/shared/bunny.off", V, F);
+    
+    // Plot the mesh
+    igl::viewer::Viewer viewer;
+    viewer.data.set_mesh(V, F);
+    viewer.launch();
+}
